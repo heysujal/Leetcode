0eb7fef6e9c3e206b0c9c8dfad443b9ba912f906
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-07-10 20:19:05 +0530

    Added solution - LeetHub

---
+++ Is Binary Tree Heap - GFG/is-binary-tree-heap.cpp
@@ -0,0 +1,188 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+// User Function template for C++
+
+// Structure of node
+/*struct Node {
+    int data;
+    Node *left;
+    
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+  
+  int countNodes(struct Node* root){
+      
+      if(root == NULL)
+        return 0 ;
+        
+        
+      return 1 + countNodes(root -> left) + countNodes(root -> right);
+      
+  }
+  
+  bool isCBT(struct Node* root , int i, int countNodes){
+      
+      if(root==NULL)
+        return true;
+        
+      if(i >= countNodes)
+        return false;
+        
+      else{
+         return isCBT(root -> left , 2*i+1, countNodes)
+                    and 
+                    isCBT(root -> right , 2*i+2, countNodes)
+                  ;
+                  
+      }
+      
+  }
+  
+  bool isMaxOrder(struct Node* root ){
+      
+        //
+      
+      
+      if(root->left == NULL and root->right==NULL)
+            return true;
+            
+        if(root->right==NULL)
+               {
+                 return  root->left->data < root->data;
+                   
+               }
+        
+        
+      
+      
+        return  root->data > root->left->data
+      
+                and 
+                root -> data > root-> right->data
+                and 
+                isMaxOrder(root->right)
+                and
+                isMaxOrder(root->left);
+                
+      
+  }
+  
+    bool isHeap(struct Node* root) {
+        // code here
+        int totalCount = countNodes(root);
+        
+        if(root == NULL)
+         return true;
+        // if both CBT and max order property
+        
+        return isCBT(root, 0 , totalCount) and isMaxOrder(root);
+        
+        
+        
+        
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        if (ob.isHeap(root))
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
