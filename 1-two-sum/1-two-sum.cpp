bde15e878386682a692b9568300b331ae1c73ce5
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-07-17 12:11:33 +0530

    Time: 165 ms (37.98%), Space: 10.3 MB (71.85%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+        
+        int n = nums.size();
+         
+        for(int i  = 0 ; i < n  ; i++){
+            
+            int toFind = target - nums[i];
+            
+            std::vector<int>::iterator  it;
+            
+            it = find(nums.begin() +1+i, nums.end() , toFind);
+            
+            if(it != nums.end()){
+                int idx = it - nums.begin();
+                return {idx, i};
+                
+            }
+            
+            
+            
+        }
+        
+        return {-1, -1};
+        
+        
+    }
+};
