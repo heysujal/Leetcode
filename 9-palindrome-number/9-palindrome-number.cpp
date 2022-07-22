f2ff08a24027e9429f78ef64188f3f90e031c939
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-07-22 21:35:19 +0530

    Time: 19 ms (65.75%), Space: 5.9 MB (31.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -4,14 +4,24 @@ public:
         if(x<0)
             return false;
         
-         string s= to_string(x);
-         
+          string s = to_string(x);
         
-        reverse(s.begin(), s.end());
-       
+        int i  =0;
+        int j = s.size() - 1;
         
-        return s == to_string(x) ;
+        while(i <= s.size() /2 and j >= s.size()/2)
+        {
+            if(s[i] == s[j])
+            {
+                i++;
+                j--;
+                
+            }else{
+        return false;
+                
+            }
+        }
         
-        
+        return true;
     }
 };
