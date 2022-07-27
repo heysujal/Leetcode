888b1787bf2a19ec79ce37d7f75d8d6ee8d1f96c
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-07-27 19:57:38 +0530

    Time: 50 ms (41.13%), Space: 9 MB (91.31%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+      void  solve(vector<int> &nums,int i, int n , int k , vector<int> &subset, vector<vector<int>>&ans )
+        
+    {         
+         if(k==0 )
+        {
+            ans.push_back(subset);     
+              return;
+        }
+            
+        if(i==n)
+            return;
+      
+       //pick
+       subset.push_back(nums[i]);
+       solve( nums,  i+1, n , k-1, subset, ans);
+       subset.pop_back();
+       
+       // skip
+      
+       solve(nums,i+1, n , k ,subset, ans);
+        
+        
+        
+        
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int> subset;
+        vector<vector<int>> ans;
+        
+        vector<int> nums(n);
+        iota(nums.begin(), nums.end(), 1);
+        
+        solve(nums ,0, n ,k , subset, ans);
+        return ans;
+        
+    }
+};
