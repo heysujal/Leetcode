0f418344f1ce547c5d8de962b06b4fe25b932e3d
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-08-02 16:33:20 +0530

    Time: 11 ms (80.53%), Space: 14 MB (47.49%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    void fill(int i , int j, vector<vector<int>>& image, int oldcolor, int newcolor, int m , int n)
+    {
+        
+        
+        if( i < 0 or j < 0 or i >= n or j>=m or image[i][j]!=oldcolor)
+            return;
+        
+        
+        // otherwise
+        image[i][j] = newcolor;      
+        fill(i+1, j , image, oldcolor, newcolor, m , n);
+        fill(i-1, j , image, oldcolor, newcolor, m , n);
+        fill(i, j+1 , image, oldcolor, newcolor, m , n);
+        fill(i, j-1 , image, oldcolor, newcolor, m , n);
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newcolor) {
+        
+        int oldcolor = image[sr][sc];
+        if(oldcolor == newcolor)
+            return image;
+        
+        
+        fill(sr, sc , image,  oldcolor, newcolor, image[0].size() , image.size());
+        return image;
+        
+    }
+};
