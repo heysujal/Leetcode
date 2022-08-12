3266b0f4a889ea7187a337ca5132f0d3f33e7513
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-08-12 20:14:17 +0530

    Time: 0 ms (100.00%), Space: 7 MB (72.33%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -10,39 +10,21 @@
  */
 class Solution {
 public:
-    int getLen(ListNode* head)
-{
-    int len = 0;
-
-    if(head==NULL)
-        return len;
-
-    while (head!=NULL)
-    {
-        head=head->next;
-        len++;
-
-    }
-    return len;
-
-    
-}
-ListNode* getMiddle(ListNode* head)
-{
-    int toTraverse = getLen(head)/2;
-    ListNode* curr = head;
-    while(toTraverse--)
-    {
-        curr = curr->next;
-
-    }
-
-    return curr;
-}
-    
-    
     ListNode* middleNode(ListNode* head) {
-        return getMiddle(head);
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast!=NULL and fast->next!=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+        }
+        
+        
+        return slow;
+
         
     }
 };
