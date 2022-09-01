285d3b6858951cf0e6e21d03d9cef27ebd0690b0
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-01 14:16:56 +0530

    Time: 7 ms (40.23%), Space: 8.1 MB (18.94%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    
-    
-    
-    
-     void perm(vector<int>& nums , vector<vector<int>> &ans , int idx)
-        
-    {
-        
-        if(idx == nums.size())
-        {
-            ans.push_back(nums);
-            return ;
-        }
-        
-        
-        
-        for(int i = idx ; i < nums.size() ; i++)
-        {
-            swap(nums[i], nums[idx]);
-            perm(nums, ans, idx+1);
-            swap(nums[i], nums[idx]);
-            
-            
-        }
-        
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> output; // to hold result
+        generatePermutations(nums, output, 0);
+        return output; // return answer
     }
-    
-    
-    vector<vector<int>> permute (vector<int>& nums) {
-        vector<vector<int>> ans;
-        perm(nums, ans , 0);
-        return ans;
+private:
+    void generatePermutations(vector<int> nums, vector<vector<int>>&output, int idx) {
+        if (idx == size(nums)) // base case
+		{ 
+            output.emplace_back(nums);
+        }
+        for (int i = idx; i < size(nums); ++i) 
+		{
+            swap(nums[i], nums[idx]); // swap the current element with the next to get another permutation
+            generatePermutations(nums, output, idx + 1); // generate permutations for the next element
+        }
     }
-    
 };
