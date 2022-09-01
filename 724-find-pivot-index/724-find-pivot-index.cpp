a0756217d8c05cc6e4c82aa36051d363ceccb026
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-02 01:36:50 +0530

    Time: 75 ms (7.83%), Space: 31.1 MB (87.52%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,46 +2,26 @@ class Solution {
 public:
     
     
-    bool solve(int idx, int left , int right, int n , vector<int> &nums)
-        
-    {
-        int leftSum = 0;
-        int rightSum = 0;
-        
-        if(left < 0)
-            leftSum = 0;
-        else
-        {
-             for(int p = 0; p <= left ; p++)
-                leftSum += nums[p];
-        }
-        
-        if(right >= n)
-            rightSum = 0;
-        
-        else{
-           
-            for(int q = right; q <= n-1 ; q++)
-                rightSum+=nums[q];
-        }
-        
-        
-        return (leftSum==rightSum);
-        
-    
-    }
-    
-    
     int pivotIndex(vector<int>& nums) {
         
-        int n = nums.size();
-        int i =0 ;
         
-        for(auto &mid : nums)
-        {
-            if(solve(i , i-1, i+1 , n, nums))
+        int n = nums.size();
+        int sum = 0;
+        
+        for(int i= 0; i < n ; i++)
+            sum += nums[i];
+        
+        int leftSum = 0 , rightSum =sum ;
+        
+        for(int i = 0 ; i < n ; i++)
+        {  
+            rightSum -= nums[i];
+          
+            if(leftSum == rightSum )
                 return i;
-            i++;
+            
+            leftSum += nums[i];
+            
         }
         return -1;
         
