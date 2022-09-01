100c6e35d8c5f31491ca6447b70931040262b534
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-01 15:19:29 +0530

    Time: 35 ms (66.85%), Space: 18 MB (33.95%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     
-      void  solve(vector<int> &nums,int i, int n , int k , vector<int> &subset, vector<vector<int>>&ans )
-        
-    {         
-         if(k==0 )
+    
+    void solve(int i , int n,int k ,vector<int> &temp,vector<vector<int>> &ans )
+    {
+        if(k==0)
         {
-            ans.push_back(subset);     
-              return;
+            ans.push_back(temp);
+            return;
+        
         }
-            
+        
+        
+        
         if(i==n)
             return;
-        if(k > n-i)
-            return ;
-      
-       //pick
-       subset.push_back(nums[i]);
-       solve( nums,  i+1, n , k-1, subset, ans);
-       subset.pop_back();
-       
-       // skip
-      
-       solve(nums,i+1, n , k ,subset, ans);
         
         
+        // pick
+        
+        temp.push_back(i+1);
+        solve(i+1, n, k-1, temp, ans);
+        temp.pop_back();
+        
+        // skip
+        
+        solve(i+1, n , k ,temp, ans);
         
         
     }
     
     vector<vector<int>> combine(int n, int k) {
         
-        vector<int> subset;
+        vector<int> temp;
         vector<vector<int>> ans;
         
-        vector<int> nums(n);
-        iota(nums.begin(), nums.end(), 1);
+        int i = 0;
+        solve(i , n,k, temp, ans);
         
-        solve(nums ,0, n ,k , subset, ans);
         return ans;
-        
     }
 };
