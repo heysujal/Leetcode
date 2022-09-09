9dc1e283eb5832db5484b549dc831e5b20aeaeda
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-10 01:28:55 +0530

    Time: 3 ms (62.91%), Space: 6 MB (40.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,16 +4,17 @@ public:
         if(n<=1)
             return n;
         
-        vector<int> dp(n, 0);
+        int f = 0;
+        int s= 1;
         
-        dp[1] = 1;
-        
-        
-        for(int i = 2; i < n ; i++ )
-            dp[i] = dp[i-1] + dp[i-2];
-        
-        
-        return dp[n-1] + dp[n-2];
+        for(int i = 2; i <= n; i++)
+        {
+            int sum = f+s;
+            f = s;
+            s= sum;
+            
+        }
+        return s;
         
     }
 };
