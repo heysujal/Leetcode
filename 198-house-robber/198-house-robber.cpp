6fdfa16225e65538fb4636064d767f89f58be685
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-12 14:43:10 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,43 +3,36 @@ public:
     
     
     
-    
-    // lets solve dp from [0...index]
-    
-    int solve(int i, vector<int> &nums, vector<int>&dp)
-    {
-        
-        if(i==0)
-        {
-            return nums[0];
-        }
-        
-        if ( i < 0)
-            return 0;
-        
-        if(dp[i]!=-1)
-            return dp[i];
-        
-        
-        int pick = nums[i] + solve(i-2, nums, dp);
-        
-        int skip  = solve(i-1, nums, dp);
-        
-        return dp[i] = max(pick , skip);
      
-    }
-    
     
     
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
+       
+        // I am solving as [0...index]
         
-        // get answer of 0...n-1
-       return solve(n-1, nums, dp);
+        // base case
         
+        dp[0] = nums[0];
         
+      for(int i = 1; i < n ; i++)
+      {
+          
+          int pick = nums[i] ;
+          if(i>1)
+              pick += dp[i-2];
+        
+          int skip  = dp[i-1];
+        
+          dp[i] = max(pick , skip);
+     
+          
+          
+      }
+        
+        return dp[n-1];
         
     }
 };
