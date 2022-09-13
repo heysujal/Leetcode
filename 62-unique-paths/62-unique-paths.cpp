bf0675f327e206865bf4fc06ca31dffc5132f9c3
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-13 19:46:56 +0530

    Time: 6 ms (12.36%), Space: 6.6 MB (27.22%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
+    // Memoization
     
-    int solve(int i , int j , int m , int n,  vector<vector<int>> &dp)
+    // solve is a function which gives answer of going 
+//         [i-1][j-1] to [0][0]
+    
+    int solve(int i , int j, vector<vector<int>> &dp)
     {
-        if(i>=m or j >= n)
+        if(i<0 or j <0)
             return 0;
-        if(i==m-1 and j==n-1)
-        {
-              
+        if(i==0 and j==0)
             return 1;
-            
-        }
         
-        if(dp[i][j] != -1)
+        if(dp[i][j]!=-1)
             return dp[i][j];
         
+        int up = solve(i-1,j, dp);
+        int left = solve(i , j-1, dp);
         
-        int down = solve(i+1, j , m , n , dp);
-        int right = solve(i, j+1, m , n , dp);
-        
-        return dp[i][j] = right + down;
-        
-        
-            
+        return dp[i][j] = up + left;
         
         
-          
-         
+        
     }
+    
+    
+    
     int uniquePaths(int m, int n) {
-        
-       vector<vector<int>> dp(m , vector<int>(n , -1));
-        return solve(0 , 0, m , n , dp);
- 
-        
-       
+            vector<vector<int>> dp(m, vector<int>(n, -1));
+            return solve(m-1, n-1, dp);
     }
 };
