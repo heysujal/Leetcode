e6ea6cf373adad6a568fe7c9bf094598bb935df2
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-13 19:57:34 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.76%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -5,7 +5,7 @@ public:
     // solve is a function which gives answer of going 
 //         [i-1][j-1] to [0][0]
     
-    int solve(int i , int j, vector<vector<int>> &dp)
+    int solveMemo(int i , int j, vector<vector<int>> &dp)
     {
         if(i<0 or j <0)
             return 0;
@@ -15,8 +15,8 @@ public:
         if(dp[i][j]!=-1)
             return dp[i][j];
         
-        int up = solve(i-1,j, dp);
-        int left = solve(i , j-1, dp);
+        int up = solveMemo(i-1,j, dp);
+        int left = solveMemo(i , j-1, dp);
         
         return dp[i][j] = up + left;
         
@@ -26,8 +26,61 @@ public:
     
     
     
+    int solveTab(int m , int n)
+    {
+        
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+         
+        
+        
+        for(int i = 0; i < m ; i++)
+        {
+            
+            
+            for(int j = 0;  j < n ; j++)
+            {
+                
+                if(i==0 and j==0) dp[i][j] = 1;
+                
+                else
+                {
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i>0)
+                        up = dp[i-1][j];
+                    if(j>0)
+                        left = dp[i][j-1];
+                    
+                    dp[i][j] = up + left;
+                }
+                
+                
+            }
+        }
+        
+        
+        
+        return dp[m-1][n-1];
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     int uniquePaths(int m, int n) {
-            vector<vector<int>> dp(m, vector<int>(n, -1));
-            return solve(m-1, n-1, dp);
+            // vector<vector<int>> dp(m, vector<int>(n, -1));
+            // return solveMemo(m-1, n-1, dp);
+        
+        
+        
+        // tabulation
+        return solveTab(m, n);
+        
+        
     }
 };
