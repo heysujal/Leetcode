13d3ef6a9a83007147ce0e0372ec8f99829540d6
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-14 13:24:34 +0530

    Time: 9 ms (22.84%), Space: 8 MB (13.34%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -21,11 +21,56 @@ public:
         
     }
     
+    int solveTab(int m , int n, vector<vector<int>> &grid)
+    {
+        
+        vector<vector<int>> dp(m , vector<int>(n, 0));
+         
+        for(int i =0 ; i < m ; i++)
+        {
+            for(int j = 0; j < n ; j++)
+            {
+                 
+                if(!i and !j and !grid[i][j]) 
+                    dp[i][j] = 1;
+                else if(!i and !j and grid[i][j])
+                    dp[i][j] = 0;
+                else if(grid[i][j] == 1) 
+                    dp[i][j] = 0;
+                else{
+                    
+                    int up = 0;
+                if(i>0)
+                    up = dp[i-1][j];
+                
+                int left =0;
+                if(j>0) 
+                    left = dp[i][j-1] ;
+        
+                 dp[i][j] = up + left;
+                    
+                    
+                }
+                
+                
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
+    }
+    
+    
+    
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
-        return solveMemo(m-1, n-1, dp, grid);
+        // return solveMemo(m-1, n-1, dp, grid);
+        
+        return solveTab(m, n , grid);
+        
     }
 };
