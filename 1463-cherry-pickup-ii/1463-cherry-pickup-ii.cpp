9378199cd3761373f02ed0ef907d02a7fdd7d41e
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-09-24 17:39:54 +0530

    Time: 445 ms (11.13%), Space: 16.9 MB (67.55%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -97,14 +97,98 @@ public:
            return dp[i][j1][j2] = maxi;      
     }
     
+    
+    int solveTab(vector<vector<int>> &grid)
+    {
+        
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m , vector<vector<int>>(n, vector<int>(n, -1)));
+        
+        
+        for(int j1= 0; j1 <n ; j1++)
+        {
+            
+            for(int j2 = 0; j2 < n ; j2++)
+            {
+
+                if(j1 == j2)
+                {
+                    dp[m-1][j1][j2] = grid[m-1][j2];
+                    
+                }
+                else{
+                    dp[m-1][j1][j2] = grid[m-1][j2]  + grid[m-1][j1];
+                    
+                }
+            
+            }
+        }
+        
+        
+        for(int i = m-2; i>=0 ; i--)
+        {
+            
+            
+            for(int j1 = 0 ; j1<n ; j1++)
+            {
+                for(int j2 = 0; j2 < n; j2++)
+                {
+                    int maxi = -1e8;
+                    for(int dj1 = -1 ; dj1<=1; dj1++)
+                    {
+
+                        for(int dj2 = -1 ; dj2<=1; dj2++)
+                        {
+                            int value;
+                             
+                            if(j1==j2)
+                                value = grid[i][j1];
+                            else 
+                                value = grid[i][j1] + grid[i][j2];
+    
+                            if(j1+dj1 >=0 and j1+dj1 < n and j2+dj2>=0 and j2+dj2 < n) 
+                                value += dp[i+1][j1 + dj1][j2 + dj2];
+                            else
+                                value = -1e8;
+                            
+                             
+                            maxi = max(maxi, value);
+ 
+                        }
+
+
+                    }
+                  
+                  dp[i][j1][j2] = maxi; 
+                     
+                    
+                    
+                    
+                }
+            }
+            
+        }
+        
+        
+        return dp[0][0][n-1];
+        
+        
+    }
     int cherryPickup(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
         // return  solve(0, 0 ,n-1, grid);
         
-        vector<vector<vector<int>>> dp(m , vector<vector<int>>(n, vector<int>(n, -1)));
-        return solveMemo(0, 0 , n-1, grid, dp);
+        //memo
+        // vector<vector<vector<int>>> dp(m , vector<vector<int>>(n, vector<int>(n, -1)));
+        // return solveMemo(0, 0 , n-1, grid, dp);
+        
+        //tabulation
+        
+        return solveTab(grid);
         
         
         
