0cd02e89b9b54472983450bbd5dc42e092809907
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2022-10-11 22:12:02 +0530

    Time: 2 ms (43.49%), Space: 6.2 MB (15.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    
+    
+    int solve(int i , int n)
+    {
+        
+        if(i==n)
+            return 1;
+        if(i > n)
+            return 0;
+        
+        int one = solve(i + 1, n);
+        int two = solve(i + 2, n);
+        
+        return one + two;
+    }
+    
+    int solveMemo(int i , int n, vector<int> &dp)
+    {
+        
+        if(i==n)
+            return 1;
+        if(i > n)
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+            
+        
+        int one = solveMemo(i + 1, n,dp);
+        int two = solveMemo(i + 2, n,dp);
+        
+        return dp[i] = one + two;
+    }
+    
     int climbStairs(int n) {
         
-        if(n<=3)
-            return n;
+        // return solve(0 , n);
         
-        vector<int> dp(n, 0);
+        vector<int> dp(n + 1, -1);
         
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        
-        for(int i = 4; i < n ; i++ )
-            dp[i] = dp[i-1] + dp[i-2];
-        
-        
-        return dp[n-1] + dp[n-2];
-        
+        return solveMemo(0, n,dp);
         
     }
 };
