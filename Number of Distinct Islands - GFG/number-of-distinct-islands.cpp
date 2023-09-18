0ed41cc958b6e0c7de206bf48ccb52f8dd437750
Sujal Gupta <55016909+heysujal@users.noreply.github.com>
2023-09-18 22:51:42 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -11,30 +11,26 @@ class Solution {
   public:
     int m;
     int n;
-    void dfs(int i, int j, vector<vector<bool>> &vis, vector<vector<int>> &v, vector<vector<int>> &grid, int x0, int y0){
+    void dfs(int i, int j, vector<vector<int>> &v, vector<vector<int>> &grid, int x0, int y0){
         if(i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != 1)
             return;
-        // vis[i][j] = true;
         grid[i][j] = 2;
         v.push_back({i-x0, j-y0});
-        dfs(i-1,j,vis,v,grid,x0,y0);
-        dfs(i+1,j,vis,v,grid,x0,y0);
-        dfs(i,j-1,vis,v,grid,x0,y0);
-        dfs(i,j+1,vis,v,grid,x0,y0);
+        dfs(i-1,j,v,grid,x0,y0);
+        dfs(i+1,j,v,grid,x0,y0);
+        dfs(i,j-1,v,grid,x0,y0);
+        dfs(i,j+1,v,grid,x0,y0);
     }
     int countDistinctIslands(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
         set<vector<vector<int>>> st;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] != 2){
                     if(grid[i][j] == 1){
                         vector<vector<int>> v;
-                        dfs(i,j,vis,v,grid,i,j);
+                        dfs(i,j,v,grid,i,j);
                         st.insert(v);
-                    }
                 }
             }
         }
